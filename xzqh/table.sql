6fac0f6aa61276e5272666044c5c9e0d1bd38b0b
zhuang jinxin <zhuangjinxin@hotmail.com>
2019-11-16 21:41:08 +0800

    修改错别字 comment

--- xzqh/table.sql
+++ xzqh/table.sql
@@ -1,13 +1,13 @@
-CREATE TABLE xzqh(
-  `id` int NOT NULL AUTO_INCREMENT COMMEMT '自增ID',
-  `code` int COMMEMT '行政区划代码',
-  `name` varchar(32) COMMEMT '名称',
-  `short_name` varchar(32) COMMEMT '简称',
-  `pinyin` varchar(128) COMMEMT '拼音',
-  `area_code` varchar(4) COMMEMT '长途区号',
-  `zip_code` varchar(6) COMMEMT '邮政编码',
-  `level` tinyint COMMEMT '层级',
-  `sort` tinyint COMMEMT '顺序',
-  `parent_code` int COMMEMT '父级行政区划代码',
+  CREATE TABLE xzqh(
+  `id` int NOT NULL AUTO_INCREMENT COMMENT '自增ID',
+  `code` int COMMENT '行政区划代码',
+  `name` varchar(32) COMMENT '名称',
+  `short_name` varchar(32) COMMENT '简称',
+  `pinyin` varchar(128) COMMENT '拼音',
+  `area_code` varchar(4) COMMENT '长途区号',
+  `zip_code` varchar(6) COMMENT '邮政编码',
+  `level` tinyint COMMENT '层级',
+  `sort` tinyint COMMENT '顺序',
+  `parent_code` int COMMENT '父级行政区划代码',
   PRIMARY KEY (`id`)
 ) COMMENT '行政区划表';
